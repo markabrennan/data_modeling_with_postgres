361ba379c2eece38fef250b473a4cbba7142ccb9
markabrennan <markangusbrennan@gmail.com>
2020-06-20 17:17:17 -0400

    added some comments on attribution.

--- create_tables.py
+++ create_tables.py
@@ -4,6 +4,9 @@ the main database - sparkifydb - as well as running queries
 to drop and create five tables used for the data model.
 
 TO DO:  Add more exception handling!
+
+NOTE: Most of these functions come from the project template;
+I have added logging and configuration.
 """
 
 import psycopg2
@@ -16,6 +19,9 @@ import config_mgr
 def create_database(cfg):
     """Create the database, after first dropping it 
     (if it exists).
+    NOTE: This function and its steps come from the 
+    project template!
+
     Args:       cfg is an instance of the ConfigMgr class
                 which has config info.
     Returns:    DB cursor and connection.
