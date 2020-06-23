b1e80fbabb10c553e74f1c438b5cec19b4bd92f1
markabrennan <markangusbrennan@gmail.com>
2020-06-23 13:29:20 -0400

    Updated data model.

--- data_model.sql
+++ data_model.sql
@@ -5,18 +5,17 @@ are already contained in the sql_queries.py, but they
 are included here for completeness.)
 """
 
-
 CREATE TABLE IF NOT EXISTS songs (
     song_id varchar PRIMARY KEY,
-    title varchar,
-    artist_id varchar,
-    year int,
-    duration decimal
+    title varchar NOT NULL,
+    artist_id varchar NOT NULL,
+    year int NOT NULL,
+    duration decimal NOT NULL
 )
 
 CREATE TABLE IF NOT EXISTS artists(
     artist_id varchar PRIMARY KEY,
-    name varchar,
+    name varchar NOT NULL,
     location varchar,
     latitude varchar,
     longitude varchar
@@ -24,30 +23,29 @@ CREATE TABLE IF NOT EXISTS artists(
 
 CREATE TABLE IF NOT EXISTS time(
     timestamp timestamp PRIMARY KEY,
-    hour int,
-    day int,
-    week int,
-    month int,
-    year int,
-    weekday int
+    hour int NOT NULL,
+    day int NOT NULL,
+    week int NOT NULL,
+    month int NOT NULL,
+    year int NOT NULL,
+    weekday int NOT NULL
 )
 
 CREATE TABLE IF NOT EXISTS users(
-    user_id int primary key,
-    first_name varchar,
-    last_name varchar,
-    gender char,
-    level varchar
+    user_id int PRIMARY KEY,
+    first_name varchar NOT NULL,
+    last_name varchar NOT NULL,
+    gender char NOT NULL,
+    level varchar NOT NULL
 )
-
 CREATE TABLE IF NOT EXISTS songplays(
     songplay_id serial PRIMARY KEY,
-    start_time timestamp,
-    user_id varchar,
-    level varchar,
+    start_time timestamp NOT NULL,
+    user_id varchar NOT NULL,
+    level varchar NOT NULL,
     song_id varchar,
     artist_id varchar,
-    session_id int,
-    location varchar,
-    user_agent varchar
+    session_id int NOT NULL,
+    location varchar NOT NULL,
+    user_agent varchar NOT NULL
 )
